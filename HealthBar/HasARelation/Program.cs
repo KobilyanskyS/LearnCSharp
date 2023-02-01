95928f1a15e101da348741eff82e957cdca73938
KobilyanskyS <kobilyansky.s@gmail.com>
2023-02-02 02:36:42 +0300

    I learned Has-a relation in C#

---
+++ HealthBar/HasARelation/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace HasARelation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Performer performer1 = new Performer("Пупа");
+            Performer performer2 = new Performer("Лупа");
+
+            Task[] tasks = { new Task(new Performer("Пупа"), "Вскопать грядку"), new Task(performer2, "Чиллить") };
+
+            Board schedule = new Board(tasks);
+
+            schedule.ShowAllTasks();
+        }
+    }
+    class Performer
+    {
+        public string Name;
+
+        public Performer(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class Board
+    {
+        public Task[] Tasks;
+
+        public Board(Task[] tasks)
+        {
+            Tasks = tasks;
+        }
+
+        public void ShowAllTasks()
+        {
+            for (int i = 0; i < Tasks.Length; i++)
+            {
+                Tasks[i].ShowInfo();
+            }
+        }
+
+    }
+
+    class Task
+    {
+        public Performer Worker;
+        public string Description;
+
+        public Task(Performer worker, string description)
+        {
+            Description = description;
+            Worker = worker;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Задача: {Description}\nИсполнитель: {Worker.Name}\n\n");
+        }
+    }
+}
