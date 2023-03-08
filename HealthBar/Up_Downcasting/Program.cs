09fa418dcba7241cdc490c6b2e8ac2a958342059
KobilyanskyS <kobilyansky.s@gmail.com>
2023-03-08 14:22:44 +0300

    Upcasting and downcasting, is, as, type conversion

---
+++ HealthBar/Up_Downcasting/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Up_Downcasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Student("Jim", 5);
+
+            switch (person)
+            {
+                case Mentor mentor:
+                    mentor.ShowName();
+                    Console.WriteLine(mentor.NumberOfStudents);
+                    break;
+                case Student student:
+                    student.ShowName();
+                    Console.WriteLine(student.AvarageScore);
+                    break;
+            }
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public void ShowName() 
+        { 
+            Console.WriteLine("Я - " + Name);
+        }
+    }
+
+    class Mentor : Person
+    {
+        public int NumberOfStudents
+        {
+            get; private set;
+        }
+        public Mentor(string name, int numberOfStudents) : base(name)
+        {
+            NumberOfStudents = numberOfStudents;
+        }
+    }
+
+    class Student : Person
+    {
+        public int AvarageScore { get; private set; }
+
+        public Student(string name, int avarageScore) : base(name)
+        {
+            AvarageScore = avarageScore;
+        }
+    }
+
+}
